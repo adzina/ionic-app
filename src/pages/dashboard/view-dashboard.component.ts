88aa54db103a0106765471995fffcd2bf33a19f3
adzina <adzina3@vp.pl>
2017-10-21 21:03:47 +0200

    randomizing function added

--- src/pages/dashboard/view-dashboard.component.ts
+++ src/pages/dashboard/view-dashboard.component.ts
@@ -49,40 +49,38 @@ export class DashboardComponent {
 
     //how_far mówi o ile wyrazów w lewo lub w prawo od wybranego slowa się przesuniemy
    var how_far = Math.floor(Math.random() * 4);
-   console.log("rand:"+rand);
-   console.log("how_far:"+how_far);
     this.wordToGuess = this.words[rand];
-    // var j=0;
-    // if(rand+how_far>=length){
-    //   for(var i=0;i<=rand+how_far-length;i++)
-    //   {
-    //     this.options[j]=this.words[i];
-    //     j++;
-    //   }
-    //   for(var i=rand-(3-how_far);i<length;i++){
-    //     this.options[j]=this.words[i];
-    //     j++;
-    //   }
-    // }
-    // else if(rand-(3-how_far)<0){
-    //   for(var i=rand;(i<rand+(3-how_far) && i<length);i++){
-    //     this.options[j]=this.words[i];
-    //     j++;
-    //   }
-    //   for(var i=0;i<rand;i++){
-    //     this.options[j]=this.words[i];
-    //     j++;
-    //   }
-    // }
-    // else{
-    //       for(var i=rand-(3-how_far);i<=rand+how_far;i++){
-    //           this.options[j]=this.words[i];
-    //           j++;
-    //         }
-    //
-    //   }
+    var j=0;
+    if(rand+how_far>=length){
+      for(var i=0;i<=rand+how_far-length;i++)
+      {
+        this.options[j]=this.words[i];
+        j++;
+      }
+      for(var i=rand-(3-how_far);i<length;i++){
+        this.options[j]=this.words[i];
+        j++;
+      }
+    }
+    else if(rand-(3-how_far)<0){
+      for(var i=length+rand-(3-how_far); i<length;i++){
+        this.options[j]=this.words[i];
+        j++;
+      }
+      for(var i=0;i<rand+how_far;i++){
+        this.options[j]=this.words[i];
+        j++;
+      }
+	  if(rand==0 || how_far==0)this.options[j]=this.words[rand];
+    }
+    else{
+          for(var i=rand-(3-how_far);i<=rand+how_far;i++){
+              this.options[j]=this.words[i];
+              j++;
+            }
+
+      }
       this.wordsReady=true;
-      this.options=this.words;
   }
   assign(x: string) {
     if (!this.clicked) {
