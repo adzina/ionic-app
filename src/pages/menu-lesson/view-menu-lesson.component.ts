d60dbfbbfa5d3c6c962df9e6e8ea6009ebd06f4c
adzina <adzina3@vp.pl>
2018-04-22 13:54:23 +0200

    menu changed

--- src/pages/menu-lesson/view-menu-lesson.component.ts
+++ src/pages/menu-lesson/view-menu-lesson.component.ts
@@ -4,7 +4,7 @@ import { ToastController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 import { UserService } from '../../services/user.service';
 import { BackendService } from '../../services/backend.service';
-import { ChooseRevisionLearningComponent } from '../choose-revision-learning/view-choose-revision-learning.component';
+import { ProgressComponent } from '../progress/view-progress.component';
 import { GoodbyeComponent } from '../goodbye/view-goodbye.component';
 import { Group } from '../../models/group';
 import { Lesson } from '../../models/lesson';
@@ -23,8 +23,7 @@ export class MenuLessonComponent {
     private _toast: ToastController) {
 
     this._backendService.getAllMyLessons().subscribe(data => {
-      console.log("dostalem z backend");
-      console.log(data);
+
       this._userService.setAllLessons(data);
 
     });
@@ -35,16 +34,10 @@ export class MenuLessonComponent {
 
 
   setmode(m: number) {
-    switch (m) {
-      case 0: this._userService.setModeOfResponse(0); this._userService.chooseLessonByNr([0]); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 1: this._userService.setModeOfResponse(1); this._userService.chooseLessonByNr([0]); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 2: this._userService.setModeOfResponse(0); this._userService.chooseLessonByNr([0, 1, 2]); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 3: this._userService.setModeOfResponse(1); this._userService.chooseLessonByNr([0, 1, 2]); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 4: this._userService.setModeOfResponse(0); this._userService.chooseAllLessons(); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 5: this._userService.setModeOfResponse(1); this._userService.chooseAllLessons(); this._navCtrl.push(ChooseRevisionLearningComponent); break;
-      case 6: this._navCtrl.push(ChooseLessonComponent); break;
-    }
-    this.mode = m;
+    if(m)
+      this._navCtrl.push(ProgressComponent);
+    else
+      this._navCtrl.push(ChooseLessonComponent);
   }
   logout() {
     this._navCtrl.push(GoodbyeComponent);
